204799c740e1f1b60ad9587742f88b3a9fcca4cb
zigzzzag <maximnikiforov1991@gmail.com>
2015-06-07 22:56:41 +0600

    add rotateGRTest

---
+++ src/test/java/org/zgame/tetris/RotateGRTest.java
@@ -0,0 +1,82 @@
+package org.zgame.tetris;
+
+import junit.framework.TestCase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.zgame.tetris.component.FigureType;
+import org.zgame.tetris.component.RootGlass;
+import org.zgame.tetris.component.TemplateOfFigure;
+
+import java.util.Arrays;
+
+/**
+ * Created by mnikiforov on 07.06.2015.
+ */
+public class RotateGRTest extends TestCase {
+    private static final Logger log = LoggerFactory.getLogger(RotateGRTest.class);
+
+    public void testRotateGR() {
+        byte[][] rootGlassMatr = new byte[][]{
+            //    0  1  2   3   4  5  6  7  8  9
+            /*0*/{0, 0, 0,  0,  0, 0, 0, 0, 0, 0},
+            /*1*/{0, 0, 0,  0,  0, 0, 0, 0, 0, 0},
+            /*2*/{0, 0, 0,  0,  0, 0, 0, 0, 0, 0},
+            /*3*/{0, 0, 0,  0,  0, 0, 0, 0, 0, 0},
+            /*4*/{0, 0, 0,  0,  0, 0, 0, 0, 0, 0},
+            /*5*/{0, 0, 0,  0,  0, 0, 0, 0, 0, 0},
+            /*6*/{0, 0, 0,  0,  0, 0, 0, 0, 0, 0},
+            /*7*/{0, 0, 0,  0,  0, 0, 0, 0, 0, 0},
+            /*8*/{0, 0, 0, 00, 00, 0, 0, 0, 0, 0},
+            /*9*/{0, 0, 0,  0, 00, 0, 0, 0, 0, 0},
+           /*10*/{0, 0, 0,  0, 00, 0, 0, 0, 0, 0},
+           /*11*/{0, 0, 0,  0,  0, 0, 0, 0, 0, 0},
+           /*12*/{0, 0, 0,  0,  0, 0, 0, 0, 0, 0},
+           /*13*/{0, 0, 0,  0,  0, 0, 0, 0, 0, 0},
+           /*14*/{0, 0, 0,  0,  0, 0, 0, 0, 0, 0},
+           /*15*/{0, 0, 0,  0,  0, 0, 0, 0, 0, 0},
+           /*16*/{0, 0, 0,  0,  0, 0, 0, 0, 0, 0},
+           /*17*/{0, 0, 0,  0,  0, 0, 0, 0, 0, 0},
+           /*18*/{0, 0, 0,  0,  0, 0, 0, 0, 0, 0},
+           /*19*/{0, 0, 0,  0,  0, 0, 0, 0, 0, 0}
+        };
+
+        byte[][] matrExpected = new byte[][]{
+            //    0  1  2   3   4   5  6  7  8  9
+            /*0*/{0, 0, 0,  0,  0,  0, 0, 0, 0, 0},
+            /*1*/{0, 0, 0,  0,  0,  0, 0, 0, 0, 0},
+            /*2*/{0, 0, 0,  0,  0,  0, 0, 0, 0, 0},
+            /*3*/{0, 0, 0,  0,  0,  0, 0, 0, 0, 0},
+            /*4*/{0, 0, 0,  0,  0,  0, 0, 0, 0, 0},
+            /*5*/{0, 0, 0,  0,  0,  0, 0, 0, 0, 0},
+            /*6*/{0, 0, 0,  0,  0,  0, 0, 0, 0, 0},
+            /*7*/{0, 0, 0,  0,  0,  0, 0, 0, 0, 0},
+            /*8*/{0, 0, 0,  0,  0, 01, 0, 0, 0, 0},
+            /*9*/{0, 0, 0, 01, 01, 01, 0, 0, 0, 0},
+           /*10*/{0, 0, 0,  0,  0,  0, 0, 0, 0, 0},
+           /*11*/{0, 0, 0,  0,  0,  0, 0, 0, 0, 0},
+           /*12*/{0, 0, 0,  0,  0,  0, 0, 0, 0, 0},
+           /*13*/{0, 0, 0,  0,  0,  0, 0, 0, 0, 0},
+           /*14*/{0, 0, 0,  0,  0,  0, 0, 0, 0, 0},
+           /*15*/{0, 0, 0,  0,  0,  0, 0, 0, 0, 0},
+           /*16*/{0, 0, 0,  0,  0,  0, 0, 0, 0, 0},
+           /*17*/{0, 0, 0,  0,  0,  0, 0, 0, 0, 0},
+           /*18*/{0, 0, 0,  0,  0,  0, 0, 0, 0, 0},
+           /*19*/{0, 0, 0,  0,  0,  0, 0, 0, 0, 0}
+        };
+
+        RootGlass rootGlass = new RootGlass(rootGlassMatr);
+        TemplateOfFigure tof = new TemplateOfFigure(FigureType.G_R, 8, 3);
+
+        tof.rotate(rootGlass);
+
+        if (Arrays.deepEquals(tof.getFigure(), matrExpected)) {
+            log.debug("rotateGRTest is passed!");
+            assertTrue(true);
+        } else {
+            log.error("rotateGRTest is not passed");
+            log.debug("tof.getFigure(): {}", Arrays.deepToString(tof.getFigure()));
+            log.debug("matrExpected:    {}", Arrays.deepToString(matrExpected));
+            assertTrue(false);
+        }
+    }
+}
